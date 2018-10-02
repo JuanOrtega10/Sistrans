1ba9545d210bbeea98d78a0de06c67efea2e929a
Diany Quintero <dy.quintero@uniandes.edu.co>
2018-10-02 18:59:47 -0500

    Proveedor

--- src/main/java/uniandes/isis2304/superandes/negocio/Proveedor.java
+++ src/main/java/uniandes/isis2304/superandes/negocio/Proveedor.java
@@ -1,49 +1,140 @@
-package model ;
+/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Universidad	de	los	Andes	(Bogotá	- Colombia)
+ * Departamento	de	Ingeniería	de	Sistemas	y	Computación
+ * Licenciado	bajo	el	esquema	Academic Free License versión 2.1
+ * 		
+ * Curso: isis2304 - Sistemas Transaccionales
+ * Proyecto: SuperAndes
+ * @version 1.0
+ * @author Juan Ortega - Diany Quintero
+ * Octubre de 2018
+ * 
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+package uniandes.isis2304.superandes.negocio;
 
 
 /**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
+ * Clase para modelar el concepto Proveedor del negocio de superandes.
+ *
+ * @author ja.ortega - dy.quintero.
  */
 
-public class Proveedor
+public class Proveedor implements VOProveedor
 {
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
+	/* ****************************************************************
+	 * 			Atributos
+	 *****************************************************************/
 	
+	/**
+	 * El identificador ÚNICO del Proveedor.
+	 */
 	private long nit;
 	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
 	
+	/**
+	 * @return El nombre del Proveedor
+	 */
 	private String nombre;
 	
+
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
+	 * @return La calificacion del Proveedor
 	 */
-	
 	private double calificacion;
 	
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
+	 * @return El numero de calificaciones del Proveedor
 	 */
-	public Proveedor(){
-		super();
+	private int numCalificaciones;
+	
+	
+	/* ****************************************************************
+	 * 			Métodos 
+	 *****************************************************************/
+	/**
+	 * Constructor por defecto
+	 */
+	public Proveedor()
+	{
+		this.nit = 0;
+		this.nombre = "";
+		this.calificacion = 0;
+		this.numCalificaciones = 0;
+	}
+	
+	/**
+	 * Constructor con valores
+	 * @param pNit - el nit del proveedor
+	 * @param pNombre - el nombre del proveedor
+	 * @param pCalificacion - calificacion del proveedor
+	 * @param pNumCalificaciones - numero de calificaciones del proveedor
+	 */
+	public Proveedor ( long pNit, String pNombre, double pCalificacion, int pNumCalificaciones )
+	{
+		this.nit = pNit;
+		this.nombre = pNombre;
+		this.calificacion = pCalificacion;
+		this.numCalificaciones = pNumCalificaciones;
 	}
 
+	/**
+	 * @return the nit
+	 */
+	public long getNit() {
+		return nit;
+	}
+
+	/**
+	 * @param nit the nit to set
+	 */
+	public void setNit(long nit) {
+		this.nit = nit;
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the calificacion
+	 */
+	public double getCalificacion() {
+		return calificacion;
+	}
+
+	/**
+	 * @param calificacion the calificacion to set
+	 */
+	public void setCalificacion(double calificacion) {
+		this.calificacion = calificacion;
+	}
+
+	/**
+	 * @return the numCalificaciones
+	 */
+	public int getNumCalificaciones() {
+		return numCalificaciones;
+	}
+
+	/**
+	 * @param numCalificaciones the numCalificaciones to set
+	 */
+	public void setNumCalificaciones(int numCalificaciones) {
+		this.numCalificaciones = numCalificaciones;
+	}
+	
+
+	
 }
 
---
+++ src/main/java/uniandes/isis2304/superandes/negocio/VOProveedor.java
@@ -0,0 +1,52 @@
+/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Universidad	de	los	Andes	(Bogotá	- Colombia)
+ * Departamento	de	Ingeniería	de	Sistemas	y	Computación
+ * Licenciado	bajo	el	esquema	Academic Free License versión 2.1
+ * 		
+ * Curso: isis2304 - Sistemas Transaccionales
+ * Proyecto: SuperAndes
+ * @version 1.0
+ * @author Juan Ortega - Diany Quintero
+ * Octubre de 2018
+ * 
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+package uniandes.isis2304.superandes.negocio;
+
+/**
+ * Interfaz para los métodos get de ALMACENAMIENTO.
+ * Sirve para proteger la información del negocio de posibles manipulaciones desde la interfaz 
+ * 
+ * @author ja.ortega - dy.quintero
+ */
+public interface VOProveedor 
+{
+	/* ****************************************************************
+	 * 			Métodos 
+	 *****************************************************************/
+ 
+	/**
+	 * @return the nit
+	 */
+	public long getNit();
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre();
+
+	/**
+	 * @return the calificacion
+	 */
+	public double getCalificacion();
+
+	/**
+	 * @return the numCalificaciones
+	 */
+	public int getNumCalificaciones() ;
+
+	
+	
+
+}

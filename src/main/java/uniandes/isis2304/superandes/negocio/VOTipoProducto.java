2e2463c6e6f1084572260dbe884d244255213b57
Diany Quintero <dy.quintero@uniandes.edu.co>
2018-10-02 19:22:15 -0500

    TipoProducto, Sucursal

--- src/main/java/uniandes/isis2304/superandes/negocio/VOTipoProducto.java
+++ src/main/java/uniandes/isis2304/superandes/negocio/VOTipoProducto.java
@@ -15,36 +15,22 @@
 package uniandes.isis2304.superandes.negocio;
 
 /**
- * Interfaz para los métodos get de Proveedor.
+ * Interfaz para los métodos get de Tipo Producto.
  * Sirve para proteger la información del negocio de posibles manipulaciones desde la interfaz 
  * 
  * @author ja.ortega - dy.quintero
  */
-public interface VOProveedor 
+public interface VOTipoProducto 
 {
-	/* ****************************************************************
-	 * 			Métodos 
-	 *****************************************************************/
- 
 	/**
-	 * @return the nit
+	 * @return the id
 	 */
-	public long getNit();
-
+	public long getId();
 	/**
 	 * @return the nombre
 	 */
 	public String getNombre();
-
-	/**
-	 * @return the calificacion
-	 */
-	public double getCalificacion();
-
-	/**
-	 * @return the numCalificaciones
-	 */
-	public int getNumCalificaciones() ;
+	
 
 	
 	

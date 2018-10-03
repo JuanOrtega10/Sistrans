3a6c3979d514a17b87d7f33426246632d057800c
Diany Quintero <dy.quintero@uniandes.edu.co>
2018-10-02 22:20:32 -0500

    sql

---
+++ src/main/java/uniandes/isis2304/superandes/negocio/SuperAndes.java
@@ -0,0 +1,50 @@
+/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Universidad	de	los	Andes	(Bogotá	- Colombia)
+ * Departamento	de	Ingeniería	de	Sistemas	y	Computación
+ * Licenciado	bajo	el	esquema	Academic Free License versión 2.1
+ * 		
+ * Curso: isis2304 - Sistemas Transaccionales
+ * Proyecto: SuperAndes
+ * @version 1.0
+ * @author Juan Ortega - Diany Quintero
+ * Octubre de 2018
+ * 
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+package uniandes.isis2304.superandes.negocio;
+
+import java.sql.Timestamp;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import com.google.gson.JsonObject;
+
+import uniandes.isis2304.superandes.persistencia.PersistenciaParranderos;
+
+/**
+ * Clase principal del negocio SuperAndes
+ * Satisface todos los requerimientos funcionales del negocio
+ *
+ * @author ja.ortega - dy.quintero.
+ */
+public class SuperAndes {
+
+	/* ****************************************************************
+	 * 			Constantes
+	 *****************************************************************/
+	/**
+	 * Logger para escribir la traza de la ejecución
+	 */
+	private static Logger log = Logger.getLogger(Parranderos.class.getName());
+	
+	/* ****************************************************************
+	 * 			Atributos
+	 *****************************************************************/
+	/**
+	 * El manejador de persistencia
+	 */
+	private PersistenciaParranderos pp;
+	
+}
